cfa3c262c5d1dc19743549bd8c125631902d7f40
jam999 <15010794331@sina.cn>
2017-07-20 21:40:21 +0800

    这是添加的礼物模块

--- cat/backend/controllers/GiftController.php
+++ cat/backend/controllers/GiftController.php
@@ -3,6 +3,10 @@ namespace backend\controllers;
 
 use Yii;
 use yii\web\Controller;
+use backend\models\UploadForm;
+use app\models\LbordLiwu;
+use yii\web\UploadedFile;
+use yii\data\ActiveDataProvider;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
 
@@ -10,17 +14,39 @@ class GiftController extends Controller
 {
 
 	/*
-		导航添加
+		礼物添加
 	 */
-	public function actionAdd()
-	{
-		if($data = Yii::$app->request->post()){
+    public function actionAdd()
+    {
+        $data = yii::$app->request->post();
 
-		}else{
-			return $this->render('add');
-		}
-	}
+        $model = new UploadForm();
+        if (Yii::$app->request->isPost) {
+            $model->img = UploadedFile::getInstance($model, 'img');
+            $path = $model->upload();
 
+            if (!($path == false)) {
+                // 文件上传成功
+                $LbordLiwu = new LbordLiwu();
+                $LbordLiwu->lw_name = $data['LbordLiwu']['lw_name'];
+                $LbordLiwu->lw_price = $data['LbordLiwu']['lw_price'];
+
+                $LbordLiwu->add_time = time();
+                $LbordLiwu->lw_img = $path;
+                $re = $LbordLiwu->save();
+                if ($re) {
+                    $this->redirect('?r=gift/list');
+                } else {
+                    $this->redirect('?r=gift/add');
+                    die;
+                }
+            }
+        } else {
+            $models = new LbordLiwu();
+            $model = new UploadForm();
+            return $this->render('add',['models'=>$models,'model'=>$model]);
+        }
+    }
 	/*
 		导航列表
 	 */
@@ -29,7 +55,14 @@ class GiftController extends Controller
 		if($data = Yii::$app->request->post()){
 
 		}else{
-			return $this->render('list');
+            $query = LbordLiwu::find();
+            $dataProvider = new ActiveDataProvider([
+                'query'=>$query,
+                'pagination'=>[
+                    'pagesize'=>10
+                ]
+            ]);
+            return $this->render('list',['dataProvider'=>$dataProvider]);
 		}
 	}
 
@@ -46,11 +79,22 @@ class GiftController extends Controller
 	}
 
 	/*
-		导航删除
+		礼物删除
 	 */
-	public function actionDel()
-	{
+	public function actionDelete()
+    {
+        $id = $_GET['id'];
+        $i = yii::$app->db->createCommand()->delete('lbord_liwu',"lw_id = $id")->execute();
 
-	}
+        if($i){
+            return $this->actionList();
+
+        }else{
+            echo "<script>alert('删除失败')</script>";
+            return $this->actionList();
+
+        }
+
+    }
 
 }
