c112dad09b511ae455ff73d2eba0278c31512a2e
shadow921233 <shadow921233@sina.com>
2017-07-21 22:37:31 +0800

    基于角色的权限控制

--- cat/backend/controllers/CommonController.php
+++ cat/backend/controllers/CommonController.php
@@ -4,13 +4,14 @@ namespace backend\controllers;
 use Yii;
 use DB;
 use yii\web\Controller;
-
+use yii\db\Query;
 class CommonController extends Controller
 {
 	public $admin;
 	//添加数据
 	 public function init(){
 	 	parent::init();
+	 	$this->juri();
 		$res = $this->check();
 		if($res==0){
 			$this->redirect('?r=admin/log');
@@ -26,6 +27,33 @@ class CommonController extends Controller
 		}else{
 			return 0;
 		}
+	}
+	
+	public function juri(){
+		$admin = yii::$app->session->get('admin');
+		$admin_id = $admin['admin_id'];
+		$info = (new Query())->from('urs')
+				->where(['uid'=>$admin_id])
+				->one();
+		$role_id = $info['role_id'];
+		$infos = (new Query())->from('rjs')
+				->where(['role_id'=>$role_id])
+				->all();
+				
+		$ids = array();
+		foreach($infos as $k){
+			$ids[]=$k['juri_id'];
+		}
+		$i = implode(',',$ids);
+//		$s = $i.substr(0,1);
+		print_r($i);
+//		print_r($s);
+		$in = yii::$app->db->createCommand("select * from juris where juri_id in ($i)")->queryAll();
+//		$in = (new Query())->from('juris')
+//				->where(['juri_id'=>[$i]])
+//				->all();
+		
+		
 	}
 	
 
