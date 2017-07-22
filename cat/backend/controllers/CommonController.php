96eb2e12bc7eeabbe8c9f687669a5b64c2e141d7
shadow921233 <shadow921233@sina.com>
2017-07-22 11:12:05 +0800

    后台完善，解决问题

--- cat/backend/controllers/CommonController.php
+++ cat/backend/controllers/CommonController.php
@@ -5,6 +5,7 @@ use Yii;
 use DB;
 use yii\web\Controller;
 use yii\db\Query;
+use app\models\Juris;
 class CommonController extends Controller
 {
 	public $admin;
@@ -28,7 +29,7 @@ class CommonController extends Controller
 			return 0;
 		}
 	}
-	
+	//获取权限
 	public function juri(){
 		$admin = yii::$app->session->get('admin');
 		$admin_id = $admin['admin_id'];
@@ -45,16 +46,22 @@ class CommonController extends Controller
 			$ids[]=$k['juri_id'];
 		}
 		$i = implode(',',$ids);
+//		var_dump($i);
 //		$s = $i.substr(0,1);
-		print_r($i);
+//		print_r($i);
 //		print_r($s);
-		$in = yii::$app->db->createCommand("select * from juris where juri_id in ($i)")->queryAll();
-//		$in = (new Query())->from('juris')
-//				->where(['juri_id'=>[$i]])
-//				->all();
-		
+//		$in = yii::$app->db->createCommand("select * from juris where juri_id in ($i)")->all();;
+		$po = Juris::find()->where(['in','juri_id',[1,2,4]])->asArray()->all();
+//		echo "<pre>";
+//		print_r($po);die;
+		Yii::$app->session->set($admin['admin_name'],$po);
+		$ri = yii::$app->session->get('juri');
+//		print_r($ri);
 		
 	}
-	
+	//判断收有权限
+	public function rbac(){
+		
+	}
 
 }
